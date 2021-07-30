7ad937d9f253f0191996cb3fcf107e8d7e5f1ed3
luis-londono <43869947+luis-londono@users.noreply.github.com>
2021-07-29 21:11:15 -0500

    configuration

---
+++ personal-site/src/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import luis from '../src/images/luis.png'
+
+const Header = () => {
+  return (
+    <img alt="header" src={luis} />
+  );
+};
+
+export default Header;
---
+++ personal-site/src/Intro.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Intro = () => {
+
+  return (
+    <>
+      <div>
+        <p />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Welcome to my page! My name's Luis and I'm a software developer.
+        I"ve been professionaly writing code since 2015 and absolutely love what I do!
+        <p />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Shortly after graduating from the University of Missouri-Kansas City, I found a really great job at <a id="neu" href="https://www.neuanalytics.com"> NeuAnalytics</a>
+        where I've had the opportunity to work with some amazing mentors and advance my career in the Fintech industry. I'm excited for the future and I try and push myself to become a better developer each day.
+        <p />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I love spending my time with family, hanging out with my friends, traveling the and supporting local Kansas City sports teams.
+        <p />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Thanks for visiting my page!
+
+      </div>
+    </>
+  );
+};
+
+export default Intro;
