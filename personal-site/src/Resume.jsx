1dbf02470ec0d6f646009c0b9050a2e3a54b9115
luis-londono <43869947+luis-londono@users.noreply.github.com>
2021-09-07 21:44:08 -0500

    added resume button for mobile view

--- personal-site/src/Resume.jsx
+++ personal-site/src/Resume.jsx
@@ -4,10 +4,26 @@ import resume from "../src/images/resume.pdf";
 
 const Resume = () => {
   return (
-    <div>
+    <div className="container body-content">
       <h1>Resume</h1>
       <div id="cv">
         <Iframe source={resume} />
+      </div>
+      <br />
+      <div className="container-btn">
+        <div className="center">
+          {/* <button className="btn-download" onClick={buttonClick}>
+            Download <i class="fa fa-download"></i>
+          </button> */}
+          <a
+            class="btn-download"
+            role="button"
+            href={resume}
+            download="luis_londono"
+          >
+            Download <i class="fa fa-download"></i>
+          </a>
+        </div>
       </div>
     </div>
   );
