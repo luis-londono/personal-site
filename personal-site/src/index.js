f635dfe0e37dd3b2fd5c6553f7d2bbed18fa40e5
Luis Londono <lulondono08@gmail.com>
2026-02-28 14:29:05 -0600

    npm updates

--- personal-site/src/index.js
+++ personal-site/src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./app.scss";
 import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
 );
