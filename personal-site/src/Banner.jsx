1070c5d882e28fdecfdf65c9ab1d9493dffb5479
Luis Londono <lulondono08@gmail.com>
2026-03-08 21:58:46 -0500

    banner

--- personal-site/src/Banner.jsx
+++ personal-site/src/Banner.jsx
@@ -1,9 +1,7 @@
 import banner from "../src/images/Banner_Skyline_Subtle_Smaller.png";
 
-{
-  /* Banner image generated using ChatGPT  */
-}
 const Banner = () => {
+  /* Banner image generated using ChatGPT  */
   return <img id="banner" src={banner} alt="Luis Londono" />;
 };
 
