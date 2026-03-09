79ded18d5124bba405cff0f0d3bfcb2d54dc6d35
Luis Londono <lulondono08@gmail.com>
2026-03-08 21:29:57 -0500

    uninstall react-ga

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -2,11 +2,8 @@ import "./app.scss";
 import Header from "./Header";
 import Footer from "./Footer";
 import Routing from "./Routing";
-import ReactGA from 'react-ga';
 
 function App() {
-  ReactGA.initialize('G-R3FLXSY8JP');
-
   return (
     <>
       <div id="wrapper">
