cd99031971d10e2d2c7fbd641fa4abab3f67a99f
Luis Londono <lulondono08@gmail.com>
2026-03-08 21:41:58 -0500

    code clean up

---
+++ personal-site/src/Banner.jsx
@@ -0,0 +1,10 @@
+import banner from "../src/images/Banner_Skyline_Subtle_Smaller.png";
+
+{
+  /* Banner image generated using ChatGPT  */
+}
+const Banner = () => {
+  return <img id="banner" src={banner} alt="Luis Londono" />;
+};
+
+export default Banner;
--- personal-site/src/Header.jsx
+++ personal-site/src/Header.jsx
@@ -1,27 +1,11 @@
-import { NavLink } from "react-router-dom";
-import banner from "../src/images/Banner_Skyline_Subtle_Smaller.png";
-import resume from "../src/images/resume.pdf";
+import Navigation from "./Navigation";
+import Banner from "./Banner";
 
 const Header = () => {
   return (
     <>
-      {/* Banner image generated using ChatGPT  */}
-      <img id="banner" src={banner} alt="Luis Londono" />
-      <div id="nav">
-        <nav>
-          <NavLink to="personal-site">
-            <i className="fa fa-home" aria-hidden="true"></i> Home
-          </NavLink>{" "}
-          <span className="vertical-pipe">|</span>{" "}
-          <a href={resume} target="_blank" rel="noreferrer">
-            <i className="fa fa-file" aria-hidden="true"></i> Resume
-          </a>{" "}
-          <span className="vertical-pipe">|</span>{" "}
-          <NavLink to="personal-site/contact">
-            <i className="fa fa-phone" aria-hidden="true"></i> Contact
-          </NavLink>
-        </nav>
-      </div>
+      <Banner />
+      <Navigation />
     </>
   );
 };
