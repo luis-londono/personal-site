d6472378bb5c854606db06c801e72367f5db8f5e
Luis Londono <lulondono08@gmail.com>
2026-03-12 21:16:33 -0500

    mobile banner

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,8 +1,14 @@
-import banner from "../src/images/Banner_Skyline_Subtle_Smaller.png";
+import banner from "./images/Banner_Skyline_Subtle_Smaller.png";
+import bannerMobile from "./images/Banner_Skyline_Subtle_Mobile.png";
 
 const Banner = () => {
   /* Banner image generated using ChatGPT  */
-  return <img id="banner" src={banner} alt="Luis Londono" />;
+  return (
+    <picture>
+      <source media="(max-width: 600px)" srcSet={bannerMobile} />
+      <img id="banner" src={banner} alt="Luis Londono" />
+    </picture>
+  );
 };
 
 export default Banner;
