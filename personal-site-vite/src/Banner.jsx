5ef669449f56620287ff02a919c6943ac4689893
Luis Londono <lulondono08@gmail.com>
2026-03-12 20:51:19 -0500

    fix folder structure

--- personal-site-vite/src/Banner.jsx
+++
@@ -1,8 +0,0 @@
-import banner from "../src/images/Banner_Skyline_Subtle_Smaller.png";
-
-const Banner = () => {
-  /* Banner image generated using ChatGPT  */
-  return <img id="banner" src={banner} alt="Luis Londono" />;
-};
-
-export default Banner;
